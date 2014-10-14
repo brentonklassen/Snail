b83ea4e5aa26bca4b353871a49eebc5a2b8d980f
Brenton Klassen <midwestmicro@live.com>
2014-10-14 08:15:00 -0500

    Created module for exporting confirmations to merchants

---
+++ modules/confirmations.py
@@ -0,0 +1,37 @@
+# Brenton Klassen
+# Script to export confirmations to merchants
+# Created 10/14/2014
+
+import db
+import os
+import time
+import csv
+import settings
+
+class Confirmations:
+
+	def __init__(self):
+
+		if settings.isset('bfconfirmationsdir'):
+			self.bfconfirmationsdir = settings.get('bfconfirmationsdir')
+
+	def exportBetafreshConfirmations(self):
+
+		selectQuery = '''select o.completeOrderReference,s.trackingNumber,s.dateStamp
+		from Snail.dbo.[Order] as o 
+		join Snail.dbo.Shipment as s 
+			on s.merchantID = o.merchantID and s.shortOrderReference = o.shortOrderReference 
+		where o.merchantid = 38 and datediff(day,s.dateStamp,getdate())=0'''
+
+		db.cur.execute(selectQuery)
+
+		with open(os.path.join(self.bfconfirmationsdir,time.strftime('%Y-%m-%d')+'.csv'),'w') as f:
+
+			writer = csv.writer(f)
+			for row in db.cur.fetchall():
+				writer.write(row)
+
+
+# RUN
+C = Confirmations()
+C.exportBetafreshConfirmations()
