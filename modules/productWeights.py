dffa289f351dba4b753b3291ab1355840eb7dbce
Brenton Klassen <midwestmicro@live.com>
2014-11-05 19:10:30 -0600

    Fixed productWeights module

--- modules/productWeights.py
+++ modules/productWeights.py
@@ -10,14 +10,12 @@ productWeightsPath = os.path.join(settings.get('basepath'),'productWeights.dat')
 productWeights = dict()
 
 print('Loading product weights...')
-with open(productWeightsPath) as f:
-    reader = csv.reader(f, delimiter='\t')
-    for row in reader:
-        
-        if len(row) < 3:
-            pass
-        
-        productWeights[(row[0].strip(),row[1].strip())] = row[2].strip()
+if os.path.isfile(productWeightsPath):
+    with open(productWeightsPath) as f:
+        reader = csv.reader(f, delimiter='\t')
+        for row in reader:    
+            if len(row) > 2:
+                productWeights[(row[0].strip(),row[1].strip())] = row[2].strip()
 
 
 def isset(key):
